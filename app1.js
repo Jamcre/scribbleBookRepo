1ca00c6d9280f786701c4b6e60fff7ebd3347d8d
jamcre <jmcrespo123@gmail.com>
2023-12-22 16:49:43 -0500

    origin fix 2

--- app1.js
+++ app1.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: "https://scribblebook-frontend.onrender.com",
+    origin: "*",
     credentials: true,
     allowedHeaders:
       "X-Requested-With',X-HTTP-Method-Override,Content-Type,Accept,Authorization",
